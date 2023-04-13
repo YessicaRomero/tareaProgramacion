d261d56097e7dfb9e28381fe855069a30f5bef72
Yessica Romero <124018374+YessicaRomero@users.noreply.github.com>
2023-04-12 21:42:18 -0300

    initial commit

--- multiplos.ts
+++ multiplos.ts
@@ -1,7 +1,7 @@
 import * as rls from "readline-sync";
 
 let numero: number = rls.questionInt("ingrese un numero: ");
-let numero2: number = rls.questionInt("ingrese otro numero: ");
+let numero2: number = rls.questionInt("ingrese numero final: ");
 
 for(let i =  numero ; i<numero2 ; i++){
   
